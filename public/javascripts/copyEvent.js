ed6b9771065c7cb162666ea88ec226e1ea687573
codemaster0610 <codemaster0610@gmail.com>
2022-02-03 22:08:41 +0800

    feat: DOM operate on show.handlebars #copy

--- public/javascripts/copyEvent.js
+++ public/javascripts/copyEvent.js
@@ -1,6 +1,8 @@
 function copyEvent(id) {
+    // DOM 操作選擇器，show.handlebars
     const copy = document.querySelector('#copy').innerText
+    
     navigator.clipboard.writeText(copy)
-        .then(() => alert('網址已複製'))
+        .then(() => alert('短網址已複製'))
         .catch(error => console.log(error))
 }

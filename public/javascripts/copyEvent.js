3e0716c625a0638256ad618decc6dd01f04a5527
codemaster0610 <codemaster0610@gmail.com>
2022-02-03 22:02:44 +0800

    feat: modify the copyEvent.js-Clipboard

--- public/javascripts/copyEvent.js
+++ public/javascripts/copyEvent.js
@@ -1,4 +1,6 @@
 function copyEvent(id) {
     const copy = document.querySelector('#copy').innerText
-   
+    navigator.clipboard.writeText(copy)
+        .then(() => alert('網址已複製'))
+        .catch(error => console.log(error))
 }

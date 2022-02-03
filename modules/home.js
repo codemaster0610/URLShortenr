5260cbc3a58a19508a3dd8626e0f9b40d3326a4d
codemaster0610 <codemaster0610@gmail.com>
2022-02-03 21:03:27 +0800

    refactor: modify express routes

---
+++ modules/home.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const Url = require('../../models/url')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+router.get('/:short_url', (req, res) => {
+
+  const short_url = req.params.short_url
+  return Url.findOne({ short_url })
+    .then(item => res.redirect(`${item.url}`))
+    .catch(err => console.log(err))
+
+})
+
+module.exports = router

ed8684c45fa7c6f2b3091e1221baf10b168e03ee
codemaster0610 <codemaster0610@gmail.com>
2022-02-03 21:57:23 +0800

    feat: Create a  random strings as shortUrl

--- getShortHandler.js
+++ getShortHandler.js
@@ -4,4 +4,22 @@ function getShortHandler() {
   const number = '1234567890'
 
   let container = []
+
+  //Js Array 操作使用concat & split 操作
+  container = container.concat(upperLetter.split(''))
+
+  container = container.concat(lowerLetter.split(''))
+
+  container = container.concat(number.split(''))
+
+  // 將短網址儲存於變數
+  let shortUrl = ''
+
+  for (let i = 0; i < 5; i++) {
+    const index = Math.floor(Math.random() * container.length)
+    shortUrl += container[index]
+  }
+  return shortUrl
 }
+
+module.exports = getShortHandler
